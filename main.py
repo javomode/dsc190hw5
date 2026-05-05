9916b11ebd3d07eb23786b3a12b8c63d290b6889
Jaden Vo <jaden.p.vo@gmail.com>
2026-05-05 11:45:26 -0700

    Fixed unused import error in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     print("Hello from dsc190hw5!")
     x = 10  # unused variable # fixed
     y = 1 + 2  # extra whitespace # fixed
-    print(pow(x, y))
+    print(math.pow(x, y))
 
 
 if __name__ == "__main__":
