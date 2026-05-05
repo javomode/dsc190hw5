94bd8edd65e9cc1d48068bee8c7911657b249b23
Jaden Vo <jaden.p.vo@gmail.com>
2026-05-05 11:43:57 -0700

    Fixed ruff errors in main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import math # unused import
+import math # unused import # fixed
 
 def main():
     print("Hello from dsc190hw5!")
-    x = 10  # unused variable
-    y = 1 +  2  # extra whitespace
+    x = 10  # unused variable # fixed
+    y = 1 + 2  # extra whitespace # fixed
+    print(pow(x, y))
 
 
 if __name__ == "__main__":
